70ead9ceca704560e1bd558c510e18cbd5d502a8
HAI NGUYEN DANG <haind@acaziasoft.com>
2024-05-19 23:31:59 +0700

    update template

--- userfiles/templates/kaizen/header.php
+++ userfiles/templates/kaizen/header.php
@@ -2,7 +2,6 @@
 <html prefix="og: http://ogp.me/ns#" <?php print lang_attributes(); ?>>
 
 <head>
-
   <title>{content_meta_title}</title>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -24,30 +23,5 @@
   <link rel="stylesheet" href="<?php print template_url(); ?>/assets/css/owl.theme.default.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-  <script>
-    mw.require('icon_selector.js');
-    mw.lib.require('bootstrap5js');
-    mw.lib.require('bootstrap_select');
-
-    mw.iconLoader().addIconSet('materialDesignIcons')
-      .addIconSet('fontAwesome')
-      .addIconSet('iconsMindLine')
-      .addIconSet('iconsMindSolid')
-      .addIconSet('mwIcons')
-      .addIconSet('materialIcons');
-  </script>
-
-  <script>
-    $(document).ready(function() {
-      $('.selectpicker').selectpicker();
-    });
-  </script>
-
-  <?php print get_template_stylesheet(); ?>
-
-
-
-  <?php include('template_settings.php'); ?>
   <script src="<?php print template_url(); ?>assets/js/owl.carousel.min.js"></script>
 </head>
---
+++ userfiles/templates/kaizen/modules/layouts/templates/btn/defautl.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+type: layout
+
+name: Kaizen button
+
+position: 1
+
+*/
+
+?>
+<div class="row">
+    <div class="kaizen-animation scroll-contact">
+        <div class="kaizen-button kaizen-transition">
+            <div class="kaizen-button-headline">
+                <module type="btn" button_action="submit" text="Đặt hàng ngay" />
+            </div>
+        </div>
+    </div>
+</div>
