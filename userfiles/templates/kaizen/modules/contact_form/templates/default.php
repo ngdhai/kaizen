ec65a0cc9407a946f36f5fe3c4343d0d0ebba9bb
Dang Hai Nguyen <haind@acaziasoft.com>
2024-05-22 09:43:37 +0700

    update modal

--- userfiles/templates/kaizen/modules/contact_form/templates/default.php
+++ userfiles/templates/kaizen/modules/contact_form/templates/default.php
@@ -10,25 +10,12 @@
  */
 ?>
 
-<div class="modal fade show" style="display:none;" data-bs-backdrop="static" data-bs-keyboard="false" id="msg<?php print $form_id; ?>" tabindex="-1"
-    aria-labelledby="modalLabel" role="dialog">
+<div class="modal fade" id="msg<?php print $form_id; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel"></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="justify-content-center text-center bold text-success fs-4 fw-bold py-2"><span>ĐẶT HÀNG
-                            THÀNH
-                            CÔNG!</span></div>
-                    <div class="justify-content-center text-center fs-6 py-6"><span>Cảm ơn quý khách đã đặt hàng thành
-                            công.
-                            <br>Chúng tôi sẽ liên hệ lại để xác nhận đơn hàng qua số điện thoại <span
-                                class="fw-bold">0392
-                                038 028</span></span></div>
-                </div>
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="justify-content-center text-center bold text-success fs-4 fw-bold py-2"><span>ĐẶT HÀNG THÀNH CÔNG!</span></div>
+                <div class="justify-content-center text-center fs-6 py-6"><span>Cảm ơn quý khách đã đặt hàng thành công. <br>Chúng tôi sẽ liên hệ lại để xác nhận đơn hàng qua số điện thoại <span class="fw-bold">0392 038 028</span></span></div>
                 <div class="justify-content-center text-center pt-4">
                     <button type="button" class="btn btn-success" data-bs-dismiss="modal">OK</button>
                 </div>
@@ -36,40 +23,17 @@
         </div>
     </div>
 </div>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  Launch static backdrop modal
-</button>
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
 <div class="form">
     <form id="contactform" data-form-id="<?php print $form_id ?>" name="<?php print $form_id ?>" method="post">
         <?php print csrf_form() ?>
 
         <div class="row">
             <div class="col-12">
-                <module type="custom_fields"
-                    default-fields="Họ và tên[type=text,field_size=12,show_placeholder=true], E-mail[type=email,field_size=12,show_placeholder=true], Số điện thoại[type=phone,field_size=12,show_placeholder=true], Địa chỉ[type=textarea,field_size=12,show_placeholder=true]"
-                    input_class="form-control" />
+                <module type="custom_fields" default-fields="Họ và tên[type=text,field_size=12,show_placeholder=true], E-mail[type=email,field_size=12,show_placeholder=true], Số điện thoại[type=phone,field_size=12,show_placeholder=true], Địa chỉ[type=textarea,field_size=12,show_placeholder=true]" input_class="form-control" />
             </div>
             <div class="col-12">
-                <?php if ($require_terms && $require_terms_when == 'b'): ?>
+                <?php if ($require_terms && $require_terms_when == 'b') : ?>
                     <module type="users/terms" data-for="contact_form_default" />
                 <?php endif; ?>
             </div>
