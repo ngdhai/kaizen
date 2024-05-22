eb0b934d449096c797bed74e90665b1b18e45f27
Dang Hai Nguyen <haind@acaziasoft.com>
2024-05-22 11:51:24 +0700

    update modal

--- userfiles/templates/kaizen/header.php
+++ userfiles/templates/kaizen/header.php
@@ -5,8 +5,7 @@
   <title>{content_meta_title}</title>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
   <meta property="og:title" content="{content_meta_title}" />
   <meta name="keywords" content="{content_meta_keywords}" />
   <meta name="description" content="{content_meta_description}" />
